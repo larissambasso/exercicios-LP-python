20efe05f86eff4e7425deaaee9f0b9dd86a38b04
larissambasso <larokasinha@yahoo.com.br>
2022-02-10 19:05:11 -0300

    apaguei o 2

--- exercicio-02.py
+++
@@ -1,4 +0,0 @@
-print("{}REAJUSTE SALÁRIO{}".format('-'*20, '-'*20))
-salario = float(input("qual o seu atual salario?"))
-reajuste = salario * 1.15
-print ('Seu salario ficará {:.2f} após o reajuste de 15%'.format(reajuste))

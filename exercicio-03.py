904c3bd8661ec8076d1a0c2a279bf29723429746
larissambasso <larokasinha@yahoo.com.br>
2022-02-10 19:05:27 -0300

    apaguei o 3

--- exercicio-03.py
+++
@@ -1,5 +0,0 @@
-print("{}AREA DE UM TRIANGULO{}".format('-'*20, '-'*20))
-base = int(input("qual o valor da base do triangulo?"))
-altura = int(input("qual o valor da altura do triangulo?"))
-area = (base*altura)/2
-print("A area deste triangulo é de:", area)

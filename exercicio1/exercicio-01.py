43285858b452d6da7690d8a149a8445784173690
larissambasso <larokasinha@yahoo.com.br>
2022-02-10 19:05:48 -0300

    apaguei tudo

--- exercicio1/exercicio-01.py
+++
@@ -1,4 +0,0 @@
-print ("{}AREA DE UM QUADRADO{}".format('-'*20, '-'*20))
-lado = float(input("digite o valor do lado do quadrado:"))
-area = lado * lado
-print("A area deste quadrado é igual a:", area)

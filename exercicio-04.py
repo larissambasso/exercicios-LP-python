af516392686d6700187cbf0f335d77a5902bfdde
larissambasso <larokasinha@yahoo.com.br>
2022-02-11 09:49:24 -0300

    add ex04

---
+++ exercicio-04.py
@@ -0,0 +1,4 @@
+print("{}CONVERSAO DE TEMPERATURA{}".format('-'*20, '-'*20))
+Celsius = float(input("qual a temperatura em graus Celsius?"))
+Conversao = (9*Celsius+160)/5
+print("Esta temperatura em Fahrenheit é de:", Conversao)

f915b23808037123866be6b27ccfff93065de838
Daniel Mota <danielcaldeira.motta@gmail.com>
2022-02-12 12:42:48 -0300

    nomenclatura

--- exercicio-06.py
+++ exercicio-06.py
@@ -1,1 +1,10 @@
 print ("{}TROCA DE VALORES{}".format('-'*20, '-'*20))
+
+A = int(input("insira o valor de A:"))
+B = int(input("insira o valor de B:"))
+print(A,B)
+C = B
+B = A
+A = C
+print("Ao fazermos a inversão, A vale: {} e B vale {}".format(A,B))
+

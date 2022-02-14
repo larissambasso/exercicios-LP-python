02b03a24fc2449aaf903305cf11008534402f989
larissambasso <larokasinha@yahoo.com.br>
2022-02-14 19:59:34 -0300

    mudando ex 06

--- exercicio6/exercicio-06.py
+++ exercicio6/exercicio-06.py
@@ -4,7 +4,7 @@ print ("{}TROCA DE VALORES{}".format('-'*20, '-'*20))
 A = int(input("insira o valor de A:"))
 B = int(input("insira o valor de B:"))
 print(A,B)
-C = B
-B = A
-A = C
+C = A
+A = B
+B = C
 print("Ao fazermos a inversão, A vale: {} e B vale {}".format(A,B))

2daf10366db57aa86e67e3fe015c97e7d21f5215
larissa moura basso <larissa.mbasso@outlook.com>
2022-04-04 19:29:51 -0300

    arrumando 31

--- exercicio31/exercicio-31.py
+++ exercicio31/exercicio-31.py
@@ -5,8 +5,8 @@ from os import sep
 print("{}QUADRADO DOS NÚMEROS SEQUENCIAIS{}".format('-'*20, '-'*20))
 
 print('O quadrados dos numeros entre 10 e 150 é:')
-for n in range(10, 150):
-  print((int((n*n)/n)),n*n)
+for n in range(11, 150):
+  print(n,'-',n*n)
 
 
 #tentar separar os numeros com string

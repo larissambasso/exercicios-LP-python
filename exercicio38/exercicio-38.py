75da17c48335932ae9119be55a4b03ff1a1e8156
larissa moura basso <larissa.mbasso@outlook.com>
2022-04-04 21:09:18 -0300

    exercicio

--- exercicio38/exercicio-38.py
+++ exercicio38/exercicio-38.py
@@ -1,8 +1,23 @@
 #Receba 100 números inteiros reais. Verifique e mostre o maior e o menor valor. 
 # Obs.: somente valores positivos.
 print("{}100 NUMEROS{}".format('-'*20, '-'*20))
-r = 's'
-while r=='s':
+contador = 0
+menor = 0
+maior = 0
+while contador<5:
   n = int(input("digite um numero:"))
-  r = input("deseja continuar?{} (S/N)   ").lower()
-print('Os numeros digitados foram: {}'.format(n))
+  
+  if(contador == 0): 
+    menor = n
+
+  if n>0:
+    if n>maior:
+      maior = n
+    if n<menor:
+      menor = n
+
+  contador+=1
+  
+  
+print('o numero maior é: ',maior)
+print('o menor numero é: ',menor)

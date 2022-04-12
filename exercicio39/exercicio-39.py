102e3cdcd10ff2166def36d83da7c79837f6e3ad
larissambasso <larissa.mbasso@outlook.com>
2022-04-12 11:51:09 -0300

    add ex 39

--- exercicio39/exercicio-39.py
+++ exercicio39/exercicio-39.py
@@ -2,4 +2,10 @@
 # Casa: 	1	2	3	4	...	64 
 # Qdte:	1	2	4	8	...	N
 print("{}TABULEIRO DE XADREZ{}".format('-'*20, '-'*20))
+graos = 1
+resultado = 1
 
+for casa in range (1,64+1):
+  print('casa numero:',casa)
+  resultado *= graos*graos+1
+  print(resultado) 

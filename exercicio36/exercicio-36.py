494329875fef9d0dacfa5e68e2c5cdb8753d88c3
larissa moura basso <larissa.mbasso@outlook.com>
2022-04-13 21:48:57 -0300

    jj

--- exercicio36/exercicio-36.py
+++ exercicio36/exercicio-36.py
@@ -1,8 +1,6 @@
 #Receba um número N. Calcule e mostre a série 1 + 1/1! + 1/2! + ... + 1/N!
 print("{}FATORIAL DA SOMA DAS PARTES{}".format('-'*20, '-'*20))  
-from cmath import pi, sqrt
-import math
-from unittest import result
+
 n = int(input('Digite um numero: '))
 msg = ''
 resultado=1
@@ -18,7 +16,7 @@ for c in range(1, n+1):
     msg = ' + 1/' + str(c) + '!'
      
 
-  if c==n+1:
+  if c==n:
     msg += ' = '
   print(msg, end='')
 

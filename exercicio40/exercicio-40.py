2df615bc68e64dd62e3bfc747e06c9f816f9b014
larissa moura basso <larissa.mbasso@outlook.com>
2022-04-13 23:22:08 -0300

    add

--- exercicio40/exercicio-40.py
+++ exercicio40/exercicio-40.py
@@ -1,36 +1,23 @@
 #Receba 2 números inteiros. Verifique e mostre todos os números primos existentes entre eles.
+print("{}NUMEROS PRIMOS{}".format('-'*20, '-'*20))
 n1 = int(input('Digite um numero: '))
 n2 = int(input('Digite outro numero: '))
 
-qnt = 0
-for c in range(n1, n2 + 1):
+if n2>n1:
+  for c in range (n1+1, n2):
+    if c%2!=0 and c%1==0 and c%c==0:
+      print(c)
 
-  if :
-    print('sim', end='')
+if n1>n2:
+  for c in range (n1-1, n2, -1):
+    if c%2!=0:
+      print(c)
 
-  else:
-    print('nao', end='')
-
-
-# if n2>n1:
-#   for c in range (n1+1, n2):
-#     if c%2!=0 and c%1==0 and c%c==0:
-#       print(c)
-
-# if n1>n2:
-#   for c in range (n1-1, n2, -1):
-#     if c%2!=0:
-#       print(c)
-
-# else:
-#   print('os numeros sao iguais, nao existe nenhum numero entre eles!')
+if n1==n2:
+  print('os numeros sao iguais, nao existe nenhum numero entre eles!')
 
 
 
 
-#   if n1%1==0 and n1%n1==0 and n1%2!=0 and n1>2:
-#     print('Contudo,',n1, 'é um numero primo.' )
-#   else:
-#     print('E ',n1,'NÃO é um numero primo.')
 
 #CONTINUAR
---
+++ exercicio41/exercicio-41.py
@@ -0,0 +1,2 @@
+#Mostre todas as possibilidades de 2 dados de forma que a soma tenha como resultado 7.
+print("{}NUMEROS PRIMOS{}".format('-'*20, '-'*20))

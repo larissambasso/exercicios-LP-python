62e1030a08dac7456b46980a3e4d7e105f6b70b2
larissa moura basso <larissa.mbasso@outlook.com>
2022-04-19 21:28:25 -0300

    Arrumando

--- exercicio40/exercicio-40.py
+++ exercicio40/exercicio-40.py
@@ -3,24 +3,16 @@ print("{}NUMEROS PRIMOS{}".format('-'*20, '-'*20))
 n1 = int(input('Digite um numero: '))
 n2 = int(input('Digite outro numero: '))
 
+maior = n1 if n1>n2 else n2 
+menor = n1 if n1<n2 else n2
 divisivel = 0
 
-if n2>n1:
-  for c in range (n1+1, n2):
-    if c%2==0 and c!=2 or c%3==0 and c!=3 or c%5==0 and c!=5 or c%7==0 and c!=7:
-      divisivel += 1 #nao estou usando
+for c in range (menor, maior):
+  for r in range(2,c-1):
+    if c%r==0 and c>2 and c%2==0:
+      resultado = 1
     else:
       print(c)
-
-  
-
-if n1>n2:
-  for c in range (n1-1, n2, -1):
-    if c%2==0 and c!=2 or c%3==0 and c!=3 or c%5==0 and c!=5 or c%7==0 and c!=7:
-      divisivel += 1
-    else:
-      print(c)
-
 if n1==n2:
   print('os numeros sao iguais, nao existe nenhum numero entre eles!')
 
--- exercicio41/exercicio-41.py
+++ exercicio41/exercicio-41.py
@@ -1,20 +1,28 @@
 #Mostre todas as possibilidades de 2 dados de forma que a soma tenha como resultado 7.
 
 print("{}PROBABILIDADE COM DADOS{}".format('-'*20, '-'*20))
-possibilidades = 0
-dado2 = 0
-resultado = 0
-while dado2<=6:
-  for dado in range(1,7):
+# possibilidades = 0
+# dado2 = 0
+# resultado = 0
+# while dado2<=6:
+#   for dado in range(1,7):
 
-    if dado == 6:
-      dado2 += 1
-    if dado2 != 0 and dado2!=7:
-      print(dado2, '+',dado ,'= ', end='')
-      resultado = dado2+dado
-      print(resultado)
+#     if dado == 6:
+#       dado2 += 1
+#     if dado2 != 0 and dado2!=7:
+#       print(dado2, '+',dado ,'= ', end='')
+#       resultado = dado2+dado
+#       print(resultado)
 
-    if resultado==7:
-      possibilidades += 1
+#     if resultado==7:
+#       possibilidades += 1
 
-print('O numero de possibilidades que possuem o resultado =7, é de: ',possibilidades)
+# print('O numero de possibilidades que possuem o resultado =7, é de: ',possibilidades)
+
+#oooou
+
+for dado in range(1,7):
+  for dado2 in range(1,7):
+    if dado+dado2==7:
+      print(dado,'+',dado2,'=',dado+dado2)
+    

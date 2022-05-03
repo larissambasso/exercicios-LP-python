657b7578675229a66f2415369eb7783280b92f46
larissambasso <larissa.mbasso@outlook.com>
2022-05-03 10:38:56 -0300

    com funçao

--- exercicio19/exercicio-19.py
+++ exercicio19/exercicio-19.py
@@ -1,15 +1,19 @@
 #Receba 2 valores reais. Calcule e mostre o maior deles.
+def bigger(n1,n2):
+  if n1>n2:
+    return n1
+  elif n2>n1:
+    return n2  
+
 print("{}QUAL O MAIOR NUMERO?{}".format('-'*20, '-'*20))
 n1 = float(input('Digite um numero: '))
 n2 = float(input('Digite outro numero: '))
 
 if n1>n2:
-  maior = n1
-  print('O maior numero é: ',maior)
+  print('O maior numero é: ', bigger(n1,n2))
 
 elif n2>n1:
-  maior = n2
-  print('O maior numero é: ',maior)
+  print('O maior numero é: ', bigger(n1,n2))
 
 else:
   print('Os numeros sao iguais, nao existe um maior.')

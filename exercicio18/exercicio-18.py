0e400a83901f1b18235966776eaecaa873b1bfb1
larissambasso <larissa.mbasso@outlook.com>
2022-05-03 10:19:17 -0300

    com função

--- exercicio18/exercicio-18.py
+++ exercicio18/exercicio-18.py
@@ -1,15 +1,20 @@
 #Receba 2 valores inteiros. Calcule e mostre o resultado da diferença do maior pelo menor valor.
+def diferenca(n1,n2):
+  if n1>n2:
+    return n1-n2
+  elif n2>n1:
+    return n2-n1
+  
+
 print("{}RESULTADO DA DIFERENÇA{}".format('-'*20, '-'*20))
 n1 = int(input('Digite um numero: '))
 n2 = int(input('Digite outro numero: '))
 
 if n1>n2:
-  resultado = n1-n2
-  print('A diferença do maior para o menor é de: ',resultado)
+  print('A diferença do maior para o menor é de: ',diferenca(n1,n2))
 
 elif n2>n1:
-  resultado = n2-n1
-  print('A diferença do maior para o menor é de: ',resultado)
+  print('A diferença do maior para o menor é de: ',diferenca(n1,n2))
 
 else:
   print('Os numeros sao iguais, nao existe diferença')

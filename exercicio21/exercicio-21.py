8bad250a9df39f8c92e1a786aa6d7254a2934ed0
larissambasso <larissa.mbasso@outlook.com>
2022-05-03 14:25:34 -0300

    nao consegui funçao

--- exercicio21/exercicio-21.py
+++ exercicio21/exercicio-21.py
@@ -2,19 +2,24 @@
 #Se a mÉdia for >= 6,0 exibir 'APROVADO';
 #Se a média for >= 3,0 ou < 6,0 exibir 'EXAME';
 #Se a média for < 3,0 exibir 'RETIDO'.
+def nota(p1,p2,p3,p4):
+  media = (p1+p2+p3+p4)/4
+  if media>=6:
+    return print('Sua media foi de {}, e você está aprovado'.format(nota(media)))
+  elif media >=3 and media <6:
+    return print('Sua media foi de {}, e você está de exame'.format(nota(media)))
+  else:
+    return print('Sua media foi de {}, e você está reprovado'.format(nota(media)))
+
+
+
 print("{}MÉDIA ARITIMÉTICA{}".format('-'*20, '-'*20))
 p1 = float(input('Qual o valor da primeira nota: '))
 p2 = float(input('Qual o valor da segunda nota: '))
 p3 = float(input('Qual o valor da terceira nota: '))
 p4 = float(input('Qual o valor da quarta nota: '))
 
-media = (p1+p2+p3+p4)/4
 
-if media >=6:
-  print('Sua media foi de {}, e você está aprovado'.format(media))
+print(nota(p1,p2,p3,p4),'fim')
 
-elif media >=3 and media<6:
-  print('Sua media foi de {}, e você está de exame'.format(media))
-
-else:
-  print('Sua media foi de {}, e você está reprovado'.format(media))
+#nao consegui

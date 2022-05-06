93b16d7afcfab47f119e42623273471d7f27eef9
larissambasso <larissa.mbasso@outlook.com>
2022-05-06 15:30:54 -0300

    com funçao

--- exercicio22/exercicio-22.py
+++ exercicio22/exercicio-22.py
@@ -1,4 +1,20 @@
 #Receba 3 valores obrigatoriamente em ordem crescente e um 4° valor não necessariamente em ordem. Mostre os 4 números em ordem crescente.
+def ordem (n1,n2,n3):
+    n4 = int(input('digite um 4 numero qualquer: '))
+
+    if n4>n3:
+      return('Os numeros em ordem crescente são: ',n1, n2, n3, n4)
+
+    elif n4>n2 and n4<n3:
+      return('Os numeros em ordem crescente são: ',n1, n2, n4, n3)
+
+    elif n4>n1 and n4<n2:
+      return('Os numeros em ordem crescente são: ',n1, n4, n2, n3)
+
+    else:
+      return('Os numeros em ordem crescente são: ',n4,n1,n2,n3)
+
+
 print("{}NUMEROS EM ORDEM CRESCENTE{}".format('-'*20, '-'*20))
 n1 = int(input('Digite um numero inteiro: '))
 n2 = int(input('Digite um numero inteiro maior que o primeiro: '))
@@ -13,19 +29,6 @@ else:
   else:
     print('Os valores digitados são:',n1,n2,n3)
 
-  n4 = int(input('digite um 4 numero qualquer: '))
-
-  if n4>n3:
-      maior = n4
-      print('Os numero em ordem crescente são: ',n1, n2, n3, maior)
-
-  elif n4>n2 and n4<n3:
-      print('Os numero em ordem crescente são: ',n1, n2, n4, n3)
-
-  elif n4>n1 and n4<n2:
-        print('Os numero em ordem crescente são: ', n1, n4, n2, n3)
-
-  else:
-        print('Os numero em ordem crescente são: ',n4,n1,n2,n3)
+print(ordem(n1,n2,n3))
 
 #sei que é possivel fazer com vetores.. refarei quando eu souber.

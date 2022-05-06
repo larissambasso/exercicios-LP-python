08c063652252c0c7763abf16e57b05878d1a418a
larissambasso <larissa.mbasso@outlook.com>
2022-05-06 17:14:55 -0300

    com funçao

--- exercicio28/exercicio-28.py
+++ exercicio28/exercicio-28.py
@@ -6,22 +6,27 @@
 #>= 1000	                   >= 80	               - 5%
 #Obs.: para outras condições, preço novo será igual ao preço atual.
 
+def lucro(pa,lucromensal):
+  if lucromensal<500 and pa<30:
+    pn = pa*1.10
+    print('O preço novo será de: ',pn)
+
+  if (lucromensal>=500 and lucromensal<1000) and (pa>=30 and pa<80):
+    pn = pa*1.15
+    print('O preço novo será de: ',pn)
+
+  if lucromensal>=1000 and pa>=80:
+    desconto = pa*0.05
+    pn = pa-desconto
+    print('O preço novo será de: ',pn)
+
+  else:
+    print('O preço deste produto nao alterará. O valor é de: ',pa)
+
+
 print("{}NOVO PREÇO PRODUTO{}".format('-'*20, '-'*20))
 pa = float(input('Qual o preço atual: '))
 lucromensal = float(input('Qual o lucro mensal deste produto: '))
 
-if lucromensal<500 and pa<30:
-  pn = pa*1.10
-  print('O preço novo será de: ',pn)
+lucro(pa,lucromensal)
 
-if (lucromensal>=500 and lucromensal<1000) and (pa>=30 and pa<80):
-  pn = pa*1.15
-  print('O preço novo será de: ',pn)
-
-if lucromensal>=1000 and pa>=80:
-  desconto = pa*0.05
-  pn = pa-desconto
-  print('O preço novo será de: ',pn)
-
-else:
-  print('O preço deste produto nao alterará. O valor é de: ',pa)

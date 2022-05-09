6bbd0f7f5ef173d8b6fd3ff912ff0f178abdcd4e
larissambasso <larissa.mbasso@outlook.com>
2022-05-09 17:24:09 -0300

    terminar ex 35

--- exercicio35/exercicio-35.py
+++ exercicio35/exercicio-35.py
@@ -1,15 +1,27 @@
 #Receba 2 números inteiros, verifique qual o maior entre eles. 
 #Calcule e mostre o resultado da somatória dos números ímpares entre esses valores.
+def impar(n1,n2):
+  if n1>n2:
+    maior = n1
+    print(maior)
+    for c in range(n1, n2):
+      print('os numeros entre esses dois numeros são: ', c)
+  elif n1<n2:
+    maior = n2
+    print(maior)
+    for c in range(n2-1, n1, -1):
+      print('os numeros entre esses dois numeros são: ', c)
+  else:
+    print("Os numeros sao iguais, comece o programa novamente") 
+
+
+
+
+print("{}SOMA DOS IMPARES{}".format('-'*20, '-'*20))
 n1 = int(input("digite um numero: "))
 n2 = int(input("digite outro numero: "))
 
-if n1>n2:
-  maior = n1
-  for numeros in range(maior, n2):
-    print("oi",numeros)
+impar(n1,n2)
 
-elif n2>n1:
-  maior = n2
-  print("deu certo")
-else:
-  print("Os numeros sao iguais, comece o programa novamente") 
+
+# terminar

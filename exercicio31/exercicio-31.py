5ad3edf53c931184bfd82213d6c42a65214549e1
larissambasso <larissa.mbasso@outlook.com>
2022-05-09 13:42:40 -0300

    arrumando ex 31

--- exercicio31/exercicio-31.py
+++ exercicio31/exercicio-31.py
@@ -1,12 +1,6 @@
 #Calcule e mostre o quadrado dos números entre 10 e 150.
-from os import sep
-
-
 print("{}QUADRADO DOS NÚMEROS SEQUENCIAIS{}".format('-'*20, '-'*20))
 
 print('O quadrados dos numeros entre 10 e 150 é:')
 for n in range(10, 150):
-  print((int((n*n)/n)),n*n)
-
-
-#tentar separar os numeros com string
+  print(n, '-' ,n*n)

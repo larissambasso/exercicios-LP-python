69910bc6681632350f1fc7bf465f38f78222c55d
larissambasso <larissa.mbasso@outlook.com>
2022-05-09 15:49:11 -0300

    arr

--- exercicio32/exercicio-32.py
+++ exercicio32/exercicio-32.py
@@ -1,13 +1,16 @@
 #Receba um número inteiro. Calcule e mostre o seu fatorial.
+def inteiro(n):
+  f = 1
+  for c in range(n,0,-1):
+    if c!=1:
+      print(c, end='x')
+    else:
+      print(c, end='=')
+    f*=(c)
+  print(f, end='')
+
+
 print("{}FATORIAL NUMERO INTEIRO{}".format('-'*20, '-'*20))  
-s = 1
-n = int(input('digite um numero: '))
-for c in range(1, n+1):
-  s *= c
-  print(c, end='')
-  if c<n:
-    print('x', end='')
-  if c==n:
-    print(' = ', end='')
-    c*=c
-print(s)
+
+n = int(input('Digite um numero: '))
+inteiro(n)

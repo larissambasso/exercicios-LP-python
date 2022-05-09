113b0aaf4b89d8dfa3d00b7204ecf209bdf485c4
larissambasso <larissa.mbasso@outlook.com>
2022-05-09 16:11:57 -0300

    com funçao

--- exercicio34/exercicio-34.py
+++ exercicio34/exercicio-34.py
@@ -1,6 +1,12 @@
 #Receba um número. Calcule e mostre os resultados da tabuada desse número.
+def calculoTaboada(n):
+  print("A taboada {} é:\n".format(n)) 
+  for taboada in range(1, 11):
+    print("{} x {} = {}".format(n, taboada, n*taboada))
+
+
+
 print("{}TABOADA{}".format('-'*20, '-'*20))
 n = int(input("digite um numero para ver a taboada: "))
-print("A taboada {} é:\n".format(n)) 
-for taboada in range(1, 11):
-  print("{} x {} = {}".format(n, taboada, n*taboada))
+
+calculoTaboada(n)
